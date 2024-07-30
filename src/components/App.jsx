2b0ad1d366c2b09ae19377b5e5e0f2b2c9e07a68
EvgenS-it <evgens.it101@gmail.com>
2024-07-30 20:51:22 +0100

    HW-2 react project created

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+// import { useState, useEffect } from 'react'
+import './App.css';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Hello HW-react-2</p>
+    </>
+  );
+}
+
+export default App;
